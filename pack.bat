3a7b468ac8e93c1ccd6015ec5bcd6f4a3bd928ac
DevilXD <bartosz.peszek@gmail.com>
2022-02-10 21:53:01 +0100

    Small packaging improvements

--- pack.bat
+++ pack.bat
@@ -1,5 +1,5 @@
 @echo off
-IF NOT EXIST 7z.exe GOTO EXIT
+IF NOT EXIST 7z.exe GOTO NO7Z
 IF NOT EXIST "Twitch Drops Miner.zip" GOTO NEW
 7z u "Twitch Drops Miner.zip" "Twitch Drops Miner/" -r -x!"Twitch Drops Miner/cookies.*"
 GOTO TEST
@@ -8,6 +8,9 @@ GOTO TEST
 GOTO TEST
 :TEST
 7z t "Twitch Drops Miner.zip"
+GOTO EXIT
+:NO7Z
+echo No 7z.exe detected, skipping packaging!
 GOTO EXIT
 :EXIT
 exit %errorlevel%
