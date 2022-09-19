44fc3122e0d8faa902587b62371578ae639f30c9
DevilXD <bartosz.peszek@gmail.com>
2022-09-19 22:51:42 +0200

    Improve venv setup script

--- setup_env.bat
+++ setup_env.bat
@@ -1,2 +1,6 @@
+@set /p "choice=Install PyInstaller so you can build an EXEcutable? (y/n) "
 python -m venv env
+%CD%/env/scripts/pip install wheel
 %CD%/env/scripts/pip install -r %CD%/requirements.txt
+if "%choice%" == "y" %CD%/env/scripts/pip install pyinstaller
+%CD%/env/scripts/python %CD%/env/scripts/pywin32_postinstall.py -install -silent
