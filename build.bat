c2a28854d181decbcc4f75ad8f79828c63b41e8b
DevilXD <bartosz.peszek@gmail.com>
2022-12-07 09:26:53 +0100

    Install pyinstaller automatically when not available

--- build.bat
+++ build.bat
@@ -2,4 +2,8 @@
 cls
 set dirpath=%~dp0
 if "%dirpath:~-1%" == "\" set dirpath=%dirpath:~0,-1%
+if not exist "%dirpath%\env\scripts\pyinstaller.exe" (
+    "%dirpath%\env\scripts\pip" install pyinstaller
+    "%dirpath%\env\scripts\python" "%dirpath%\env\scripts\pywin32_postinstall.py" -install -silent
+)
 "%dirpath%/env/scripts/pyinstaller" build.spec
