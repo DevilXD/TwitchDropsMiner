06b0b2088cbfd01e29aabe706def50c555725a0f
DevilXD <4180725+DevilXD@users.noreply.github.com>
2023-12-06 22:01:16 +0100

    Minor syntax improvements

--- setup_env.bat
+++ setup_env.bat
@@ -2,32 +2,34 @@
 cls
 set dirpath=%~dp0
 if "%dirpath:~-1%" == "\" set dirpath=%dirpath:~0,-1%
+
 git --version > nul
-if %errorlevel% NEQ 0 goto NOGIT
+if %errorlevel% NEQ 0 (
+    echo No git executable found in PATH!
+    echo:
+    pause
+    exit
+)
+
 if not exist "%dirpath%\env" (
     echo:
     echo Creating the env folder...
     python -m venv "%dirpath%\env"
-    if %errorlevel% NEQ 0 goto NOPYTHON
+    if %errorlevel% NEQ 0 (
+        echo:
+        echo No python executable found in PATH!
+        echo:
+        pause
+    )
 )
+
 echo:
 echo Installing requirements.txt...
 "%dirpath%\env\scripts\python" -m pip install -U pip
 "%dirpath%\env\scripts\pip" install wheel
 "%dirpath%\env\scripts\pip" install -r "%dirpath%\requirements.txt"
-goto DONE
-:NOPYTHON
-echo:
-echo No python executable found in path!
-pause
-goto END
-:NOGIT
-echo:
-echo No git executable found in path!
-pause
-goto END
-:DONE
+
 echo:
 echo All done!
+echo:
 pause
-:END
