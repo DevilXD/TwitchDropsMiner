c213bae01b2dde47407f1fcc526a02983e9d3606
DevilXD <4180725+DevilXD@users.noreply.github.com>
2023-12-06 21:58:29 +0100

    Display a message when venv doesn't exist

--- build.bat
+++ build.bat
@@ -2,8 +2,17 @@
 cls
 set dirpath=%~dp0
 if "%dirpath:~-1%" == "\" set dirpath=%dirpath:~0,-1%
+
+if not exist "%dirpath%\env" (
+    echo:
+    echo No virtual environment found! Run setup_env.bat to set it up first.
+    echo:
+    pause
+    exit
+)
+
 if not exist "%dirpath%\env\scripts\pyinstaller.exe" (
     "%dirpath%\env\scripts\pip" install pyinstaller
     "%dirpath%\env\scripts\python" "%dirpath%\env\scripts\pywin32_postinstall.py" -install -silent
 )
-"%dirpath%/env/scripts/pyinstaller" build.spec
+"%dirpath%/env/scripts/pyinstaller" "%dirpath%\build.spec"
