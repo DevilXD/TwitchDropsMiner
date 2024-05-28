398fe7c155cbcc26e61c6d62eb3f992e4ba452ed
Valentin Metz <Valentin.Metz@web.de>
2024-05-29 01:52:49 +0200

    Fixed prioritize_by_ending_soonest

--- settings.py
+++ settings.py
@@ -18,7 +18,7 @@ class SettingsFile(TypedDict):
     exclude: set[str]
     priority: list[str]
     priority_only: bool
-    prioritze_end: bool
+    prioritize_by_ending_soonest: bool
     autostart_tray: bool
     connection_quality: int
     tray_notifications: bool
@@ -30,7 +30,7 @@ default_settings: SettingsFile = {
     "exclude": set(),
     "autostart": False,
     "priority_only": True,
-    "prioritze_end": False,
+    "prioritize_by_ending_soonest": False,
     "autostart_tray": False,
     "connection_quality": 1,
     "language": DEFAULT_LANG,
@@ -54,7 +54,7 @@ class Settings:
     exclude: set[str]
     priority: list[str]
     priority_only: bool
-    prioritze_end: bool
+    prioritize_by_ending_soonest: bool
     autostart_tray: bool
     connection_quality: int
     tray_notifications: bool
