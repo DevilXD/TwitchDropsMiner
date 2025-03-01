3048159f37f9600fd74a76bbc211380d40202b39
DevilXD <4180725+DevilXD@users.noreply.github.com>
2025-03-01 10:50:25 +0100

    Minor typing fixes

--- gui.py
+++ gui.py
@@ -1446,9 +1446,7 @@ class InventoryOverview:
         ).grid(column=1, row=4, sticky="nw", padx=4)
         # Image
         campaign_image = await self._cache.get(campaign.image_url, size=(108, 144))
-        ttk.Label(
-            campaign_frame, image=campaign_image  # type: ignore[arg-type]
-        ).grid(column=0, row=1, rowspan=4)
+        ttk.Label(campaign_frame, image=campaign_image).grid(column=0, row=1, rowspan=4)
         # Drops separator
         ttk.Separator(
             campaign_frame, orient="vertical", takefocus=False
@@ -1469,7 +1467,7 @@ class InventoryOverview:
                 ttk.Label(
                     benefits_frame,
                     text=benefit.name,
-                    image=image,  # type: ignore[arg-type]
+                    image=image,
                     compound="bottom",
                 ).grid(column=i, row=0, padx=5)
             self._drops[drop.id] = label = ttk.Label(drop_frame, justify=tk.CENTER)
