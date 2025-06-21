7ec8d6c24575c3d21527e0949c2880f27f92fb83
DevilXD <4180725+DevilXD@users.noreply.github.com>
2025-06-21 19:00:09 +0200

    Use a less invasive method for grabbing user attention

--- gui.py
+++ gui.py
@@ -2245,7 +2245,7 @@ class GUIManager:
 
     def grab_attention(self, *, sound: bool = True):
         self.tray.restore()
-        self._root.focus_force()
+        self._root.focus_set()
         if sound:
             self._root.bell()
 
