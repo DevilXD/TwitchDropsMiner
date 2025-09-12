06fcf333a55e84cc604130431e4b1576c1f7a48a
KenshiHH <kenshihh@gmx.net>
2025-09-12 17:28:20 +0200

    Update inventory.py

--- inventory.py
+++ inventory.py
@@ -343,7 +343,7 @@ class DropsCampaign:
         self.id: str = data["id"]
         self.name: str = data["name"]
         self.game: Game = Game(data["game"])
-        self.linked: bool = data["self"]["isAccountConnected"]
+        self.linked: bool = True
         self.link_url: str = data["accountLinkURL"]
         # campaign's image actually comes from the game object
         # we use regex to get rid of the dimensions part (ex. ".../game_id-285x380.jpg")
