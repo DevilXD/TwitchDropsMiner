4bd455129de05a4c341ca9a93fee946341140af3
DevilXD <4180725+DevilXD@users.noreply.github.com>
2026-01-12 08:51:02 +0100

    Another fix for the spade url extraction

--- channel.py
+++ channel.py
@@ -319,8 +319,9 @@ class Channel:
         SETTINGS_PATTERN: str = (
             r'src="(https://[\w.]+/config/settings\.[0-9a-f]{32}\.js)"'
         )
+        # NOTE: "beacon" appears in the file first, so it's the most likely one to be matched.
         SPADE_PATTERN: str = (
-            r'"beacon_?url": ?"(https://video-edge-[.\w\-/]+\.ts(?:\?allow_stream=true)?)"'
+            r'"(?:beacon|spade)_?url": ?"(https://[.\w\-/]+\.ts(?:\?allow_stream=true)?)"'
         )
         async with self._twitch.request("GET", self.url) as response1:
             streamer_html: str = await response1.text(encoding="utf8")
